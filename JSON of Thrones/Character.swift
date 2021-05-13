e74f7c9a12b8625755467d4b40f926037644e6bb
Nikita Kochetov <nothsmple@mail.ru>
2021-05-13 15:59:26 +0300

    Get it started

---
+++ JSON of Thrones/Character.swift
@@ -0,0 +1,20 @@
+//
+//  Character.swift
+//  JSON of Thrones
+//
+//  Created by Татьяна Кочетова on 13.05.2021.
+//
+
+import Foundation
+
+struct Character {
+    
+    let id: Int
+    let firstName: String
+    let lastName: String
+    let fullName: String
+    let title: String
+    let family: String
+    let image: String
+    
+}
