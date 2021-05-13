a3be9cbba9f7d85a870f1628754c6334c5ca463d
Nikita Kochetov <nothsmple@mail.ru>
2021-05-13 15:16:34 +0300

    Initial Commit

---
+++ JSON of Thrones/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON of Thrones
+//
+//  Created by Татьяна Кочетова on 13.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
