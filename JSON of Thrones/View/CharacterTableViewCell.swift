ada20dc0269ce1023fdf32f2d47cf51f6ecab25f
Nikita Kochetov <nothsmple@mail.ru>
2021-05-26 22:18:17 +0300

    CharacterTableViewCell

---
+++ JSON of Thrones/View/CharacterTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CharacterTableViewCell.swift
+//  JSON of Thrones
+//
+//  Created by Татьяна Кочетова on 26.05.2021.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var characterImageView: UIImageView!
+    
+}
