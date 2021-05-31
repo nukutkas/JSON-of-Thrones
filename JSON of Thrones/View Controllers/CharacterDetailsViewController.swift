91739819ae71fb78e84d1a3666c51bf0e3e40911
Nikita Kochetov <nothsmple@mail.ru>
2021-05-31 22:22:19 +0300

    Added class CharacterDetails

---
+++ JSON of Thrones/View Controllers/CharacterDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CharacterDetailsViewController.swift
+//  JSON of Thrones
+//
+//  Created by Татьяна Кочетова on 31.05.2021.
+//
+
+import UIKit
+
+class CharacterDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
