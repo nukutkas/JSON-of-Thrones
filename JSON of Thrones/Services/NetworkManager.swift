b9132a6eeb3c4bd4f209492eaa19d56379102f80
Nikita Kochetov <nothsmple@mail.ru>
2021-06-01 22:48:39 +0300

    Added ImageManager

--- JSON of Thrones/Services/NetworkManager.swift
+++ JSON of Thrones/Services/NetworkManager.swift
@@ -36,3 +36,15 @@ class NetworkManager {
         }.resume()
     }
 }
+
+class ImageManager {
+    static var shared = ImageManager()
+    
+    private init() {}
+    
+    func fetchImage(from url: String?) -> Data? {
+        guard let stringURL = url else { return nil }
+        guard let imageURL = URL(string: stringURL) else { return nil }
+        return try? Data(contentsOf: imageURL)
+    }
+}
